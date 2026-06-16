ca65265236b059e4cbac55a18e4062c7f7393f42
jonoton <65074561+jonoton@users.noreply.github.com>
2026-06-16 15:48:37 -0400

    update

--- cpppubsub.hpp
+++ cpppubsub.hpp
@@ -35,7 +35,7 @@ namespace cpppubsub {
 
 constexpr int VERSION_MAJOR = 1;
 constexpr int VERSION_MINOR = 5;
-constexpr int VERSION_PATCH = 0;
+constexpr int VERSION_PATCH = 1;
 
 /**
  * @brief Returns the library version as a string.
